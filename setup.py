686a134dada78ab0facb31515afa6dcfc1cd2844
Abel Castillo <abelcastillomath@gmail.com>
2020-04-23 08:19:22 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jdbcviajpype',
+    version='0.0.1',
+    description='JDBC conections via JPype',
+    author='Abel Castillo',
+    packages=find_packages(),
+)
